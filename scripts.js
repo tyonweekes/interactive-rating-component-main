462a56268ae30a2ca7e8a36c7e84748f3929bbbd
tyonweekes <92998721+tyonweekes@users.noreply.github.com>
2022-08-19 18:03:56 -0400

    Finished project.
    Finished project.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,29 @@
-// let rating = document.querySelector('.rating');
-// let submitButton = document.querySelector('button');
+let oneStar = document.getElementById('1');
+let twoStars = document.getElementById('2');
+let threeStars = document.getElementById('3');
+let fourStars = document.getElementById('4');
+let fiveStars = document.getElementById('5');
+let ratingSpace = document.getElementById('selected-rating');
+
+oneStar.addEventListener('click', function() {
+    ratingSpace.textContent = '1';
+});
+
+twoStars.addEventListener('click', function() {
+    ratingSpace.textContent = '2';
+});
+
+threeStars.addEventListener('click', function() {
+    ratingSpace.textContent = '3';
+});
+
+fourStars.addEventListener('click', function() {
+    ratingSpace.textContent = '4';
+});
+
+fiveStars.addEventListener('click', function() {
+    ratingSpace.textContent = '5';
+});
 
 function submitRating() {
     let thankYouPage = document.querySelector('.main-container-thank-you');
